ca50cb9da636fb4db63e1904db2155fe1338ef03
Thiago <thiagoalves060@gmail.com>
2022-06-30 00:33:21 -0300

    first commit

---
+++ SalesWEBMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWEBMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
